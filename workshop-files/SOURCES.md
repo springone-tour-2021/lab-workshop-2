36c097ff3ef6922c8c15bfe0fa713cb8a18ecbb3
Cora Iberkleid <ciberkleid@gmail.com>
2021-05-04 17:11:39 -0400

    Fix Spring Cloud CLI install, fix deletion of app from base image, skip pull of base image for Dockerfile build in lab

--- workshop-files/SOURCES.md
+++ workshop-files/SOURCES.md
@@ -1,6 +1,9 @@
 # Spring Boot CLI (bin/spring and lib/spring-boot-cli.jar)
 https://repo.spring.io/release/org/springframework/boot/spring-boot-cli/2.4.5/spring-boot-cli-2.4.5-bin.zip
 
+# Spring Cloud CLI
+https://github.com/spring-cloud/spring-cloud-cli/archive/refs/tags/v3.0.2.zip
+
 # Dive
 https://github.com/wagoodman/dive/releases/download/v0.10.0/dive_0.10.0_linux_amd64.tar.gz
 
---
+++ workshop-instructions/setup.d/spring-cloud-cli.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Install Spring Cloud CLI
+# Note: Assumes Spring Boot CLI already present. See:
+#     workshop-files/bin/spring
+#     and
+#     workshop-files/lib/spring-boot-cli.jar
+
+VERSION=3.0.2
+curl https://codeload.github.com/spring-cloud/spring-cloud-cli/zip/refs/tags/v$VERSION -o spring-cloud-cli.zip
+unzip spring-cloud-cli.zip
+cd spring-cloud-cli-$VERSION
+./mvnw install
+spring install org.springframework.cloud:spring-cloud-cli:$VERSION
+cd ..
+rm -rf spring-cloud-cli-$VERSION
+rm spring-cloud-cli.zip
