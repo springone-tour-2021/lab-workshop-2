79cc0fe6debad661247fe50748dd993b3edeb9dc
everythingeverywhere <ivan0tarin@gmail.com>
2021-05-04 16:40:52 -0600

    preserve files

---
+++ workshop-instructions/setup.d/spring-cli.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# echo 'alias TEST1tp="/home/eduk8s/bin/spring"' >> ~/.bash_profile
+echo 'PPPPPP' >> ~/.bash_profile
+# export SDKMAN_DIR="/home/eduk8s/bin/sdkman" && curl -s "https://get.sdkman.io" | bash
+# sdk install springboot 2.2.0.BUILD-SNAPSHOT
+# sdk use springboot 2.2.0.BUILD-SNAPSHOT
+# mvn install
+# spring install org.springframework.cloud:spring-cloud-cli:2.2.0.BUILD-SNAPSHOT
+
+# echo 'alias spring="/home/eduk8s/bin/spring"' >> ~/.bash_profile
+# export SDKMAN_DIR="/home/eduk8s/bin/sdkman" && curl -s "https://get.sdkman.io" | bash
+#  source /home/eduk8s/bin/spring
+# ln -s /home/eduk8s/bin/shell-completion/bash/spring /etc/bash_completion.d/spring
+# cat /home/eduk8s/bin/spring-shell-completion/bash/spring >> ~/.bash_profile
