a98b2b0179da561a072615dd188a644382ce671a
Cora Iberkleid <ciberkleid@gmail.com>
2021-05-05 04:02:06 -0400

    skip test for Cloud CLI - to make it faster. Minor reording

--- workshop-instructions/setup.d/spring-cloud-cli.sh
+++ workshop-instructions/setup.d/spring-cloud-cli.sh
@@ -9,7 +9,8 @@ VERSION=3.0.2
 curl https://codeload.github.com/spring-cloud/spring-cloud-cli/zip/refs/tags/v$VERSION -o spring-cloud-cli.zip
 unzip spring-cloud-cli.zip
 cd spring-cloud-cli-$VERSION
-./mvnw install
+#./mvnw install -DskipTests
+./mvnw -T 1C install -Dmaven.test.skip -DskipTests -Dmaven.javadoc.skip=true
 spring install org.springframework.cloud:spring-cloud-cli:$VERSION
 cd ..
 rm -rf spring-cloud-cli-$VERSION
