fd8469f8864b4f8d9d87755269203464d55b672b
Cora Iberkleid <ciberkleid@gmail.com>
2021-05-06 19:13:50 -0400

    simplify spring-cloud-cli install to just pull jar

--- workshop-instructions/setup.d/spring-cloud-cli.sh
+++ workshop-instructions/setup.d/spring-cloud-cli.sh
@@ -6,12 +6,12 @@
 #     workshop-files/lib/spring-boot-cli.jar
 
 VERSION=3.0.2
-curl https://codeload.github.com/spring-cloud/spring-cloud-cli/zip/refs/tags/v$VERSION -o spring-cloud-cli.zip
-unzip spring-cloud-cli.zip
-cd spring-cloud-cli-$VERSION
-#./mvnw install -DskipTests
-./mvnw -T 1C install -Dmaven.test.skip -DskipTests -Dmaven.javadoc.skip=true
+#curl https://codeload.github.com/spring-cloud/spring-cloud-cli/zip/refs/tags/v$VERSION -o spring-cloud-cli.zip
+#unzip spring-cloud-cli.zip
+#cd spring-cloud-cli-$VERSION
+##./mvnw install -DskipTests
+#./mvnw -T 1C install -Dmaven.test.skip -DskipTests -Dmaven.javadoc.skip=true
 spring install org.springframework.cloud:spring-cloud-cli:$VERSION
-cd ..
-rm -rf spring-cloud-cli-$VERSION
-rm spring-cloud-cli.zip
+#cd ..
+#rm -rf spring-cloud-cli-$VERSION
+#rm spring-cloud-cli.zip
