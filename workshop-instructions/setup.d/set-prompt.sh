e1c84f70fd31b597048700c15ac12be8e0cabfae
Cora Iberkleid <ciberkleid@gmail.com>
2021-05-09 08:44:33 -0400

    fix prompt to avoid junk characters in history

--- workshop-instructions/setup.d/set-prompt.sh
+++ workshop-instructions/setup.d/set-prompt.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo 'export PS1_OLD="$PS1"' >> ~/.bash_profile
-echo 'export PS1="\e[;33m[\w]\$ \e[m "' >> ~/.bash_profile
+echo 'export PS1="\n\[\033[33m\]\w\$ \[\033[0m\]"' >> ~/.bash_profile
